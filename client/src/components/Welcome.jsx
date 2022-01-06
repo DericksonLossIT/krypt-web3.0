cce85b403c24a86a601a984a72e067ee5f9e9051
derickson.loss <derickson.loss@paylivre.com>
2022-01-06 00:37:25 -0300

    implements the transaction card in welcome page with context

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,8 +1,11 @@
+import React, { useContext } from "react";
 // import { AiFillPlayCircle } from "react-icons/ai";
 import { SiEthereum } from "react-icons/si";
 import { BsInfoCircle } from "react-icons/bs";
 
-import { Loader } from ".";
+import { TransactionContext } from "../context/TransactionContext";
+import { Loader } from "./";
+
 const companyCommonStyles =
   "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
 
@@ -18,9 +21,17 @@ const Input = ({ placeholder, name, type, value, handleChange }) => (
 );
 
 const Welcome = () => {
-  const connectWallet = () => {};
-  
-  const handleSubmit = () => {};
+  const { connectWallet, currentAccount, formData, sendTransaction, handleChange } = useContext(TransactionContext);
+
+  const handleSubmit = (e) => {
+    const { addressTo, amount, keyword, message } = formData;
+    
+    e.preventDefault();
+
+    if(!addressTo || !amount || !keyword || !message) return;
+
+    sendTransaction();
+  };
 
   return (
     <div className="flex w-full justify-center items-center">
@@ -33,13 +44,17 @@ const Welcome = () => {
             Explore the crypto world, Buy and sell cryptocurrencies easily on
             Krypto.
           </p>
-          <button
-            type="button"
-            className="flex flex-row justify-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]"
-            onClick={connectWallet}
-          >
-            <p className="text-white text-base font-semibold">Connect wallet</p>
-          </button>
+          {!currentAccount && (
+            <button
+              type="button"
+              className="flex flex-row justify-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]"
+              onClick={connectWallet}
+            >
+              <p className="text-white text-base font-semibold">
+                Connect wallet
+              </p>
+            </button>
+          )}
           <div className="grid sm:grid-cols-3 grid-cols-2 w-full mt-10">
             <div className={`rounded-tl-2xl ${companyCommonStyles}`}>
               Reliability
@@ -80,39 +95,39 @@ const Welcome = () => {
               placeholder="Address to"
               name="addressTo"
               type="text"
-              handleChange={() => {}}
+              handleChange={handleChange}
             />
             <Input
               placeholder="Amount (ETH)"
               name="amount"
               type="number"
-              handleChange={() => {}}
+              handleChange={handleChange}
             />
             <Input
               placeholder="Keyword (Gif)"
               name="keyword"
               type="text"
-              handleChange={() => {}}
+              handleChange={handleChange}
             />
             <Input
               placeholder="Enter message"
               name="message"
               type="text"
-              handleChange={() => {}}
+              handleChange={handleChange}
             />
 
             <div className="h-[1px] w-full bg-gray-400 my-2" />
 
-            {true ? (
-                <Loader />
+            {false ? (
+              <Loader />
             ) : (
-                <button
+              <button
                 type="button"
-                onclick={handleSubmit}
+                onClick={handleSubmit}
                 className="text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] rounded-full cursor-pointer"
-                >
-                    Send now
-                </button>
+              >
+                Send now
+              </button>
             )}
           </div>
         </div>
